fe89f77b4d4667296414714cc192601cc76b812c
abdulhamid <abdul.hamid@paycraftsol.com>
2021-07-25 12:35:40 +0530

    dob

--- app/src/main/java/com/lb/lawbook/profile/ProfileBasicInfoActivity.kt
+++ app/src/main/java/com/lb/lawbook/profile/ProfileBasicInfoActivity.kt
@@ -1,15 +1,26 @@
 package com.lb.lawbook.profile
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.lb.lawbook.R
 import com.lb.lawbook.databinding.ActivityProfileBasicInfoBinding
+import java.util.*
+import kotlin.collections.HashMap
+
 
 class ProfileBasicInfoActivity : AppCompatActivity() {
 
+
+    private var mYear = 0
+    private var mMonth = 0
+    private var mDay = 0
+    private var gender = ""
     private lateinit var profileViewModel: ProfileViewModel
     val db = FirebaseFirestore.getInstance()
     val auth = FirebaseAuth.getInstance().currentUser
@@ -34,8 +45,11 @@ class ProfileBasicInfoActivity : AppCompatActivity() {
                     mBinding.editTextFirstName.setText(it.get("first")?.toString())
                     mBinding.editTextLastName.setText(it.get("last")?.toString())
                     mBinding.editTextEmail.setText(it.get("email")?.toString())
-                    mBinding.editTextAge.setText(it.get("age")?.toString())
-                    mBinding.textViewGender.text = it.get("gender")?.toString()
+                    mBinding.editTextDob.setText(it.get("dob")?.toString())
+                    gender = it.get("gender")?.toString()!!
+                    if (gender.equals("male")) mBinding.genderRDG.check(R.id.radioButton_profile_gender_male)
+                    if (gender.equals("female")) mBinding.genderRDG.check(R.id.radioButton_profile_gender_female)
+                    if (gender.equals("others")) mBinding.genderRDG.check(R.id.radioButton_profile_gender_others)
 
                 } catch (e: Exception) {
                 }
@@ -47,6 +61,17 @@ class ProfileBasicInfoActivity : AppCompatActivity() {
             insertUserDocument()
 
         }
+        pickDob()
+        setGender()
+    }
+
+    private fun setGender() {
+        mBinding.genderRDG.setOnCheckedChangeListener { radioGroup, id ->
+            if (id == R.id.radioButton_profile_gender_male) gender = "male"
+            if (id == R.id.radioButton_profile_gender_female) gender = "female"
+            if (id == R.id.radioButton_profile_gender_others) gender = "others"
+        }
+
     }
 
     fun insertUserDocument() {
@@ -54,8 +79,7 @@ class ProfileBasicInfoActivity : AppCompatActivity() {
         val firstName = mBinding.editTextFirstName.text.toString()
         val lastName = mBinding.editTextLastName.text.toString()
         val email = mBinding.editTextEmail.text.toString()
-        val age = mBinding.editTextAge.text.toString()
-        val gender = mBinding.textViewGender.text.toString()
+        val dob = mBinding.editTextDob.text.toString()
 
         val basicInfo: MutableMap<String, Any> = HashMap()
         basicInfo.put("user_name", userName)
@@ -63,9 +87,35 @@ class ProfileBasicInfoActivity : AppCompatActivity() {
         basicInfo.put("first", firstName)
         basicInfo.put("last", lastName)
         basicInfo.put("email", email)
-        basicInfo.put("age", age)
+        basicInfo.put("dob", dob)
         basicInfo.put("gender", gender)
         val documentRef = db.collection("users").document(auth?.uid.toString())
         documentRef.set(basicInfo)
     }
+
+
+    fun pickDob() {
+        mBinding.buttonDob.setOnClickListener(View.OnClickListener {
+            // Get Current Date
+            // Get Current Date
+            val c: Calendar = Calendar.getInstance()
+            c.set(Calendar.YEAR, c.get(Calendar.YEAR) - 21)
+            mYear = c.get(Calendar.YEAR)
+            mMonth = c.get(Calendar.MONTH)
+            mDay = c.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(
+                    this,
+                    { view, year, monthOfYear, dayOfMonth -> mBinding.editTextDob.setText(dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year) },
+                    mYear,
+                    mMonth,
+                    mDay
+            )
+
+            datePickerDialog.datePicker.maxDate = c.timeInMillis
+            datePickerDialog.show()
+        })
+    }
+
+
 }
